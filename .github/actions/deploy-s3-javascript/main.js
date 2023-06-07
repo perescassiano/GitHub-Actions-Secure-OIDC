6c5475c46c2571607af9bafa5bca40ea15a67959
cassianoperes89 <cassianoperes89@outlook.com>
2023-06-07 16:15:40 -0300

    Deploy action to S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run () {
+    //Input values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
     core.notice('Hello from Custom JS Action')
+
+    //Upload files
+    const s3Uri = `s3://${bucket}`
+    AWS_ACCESS_KEY_ID = 
+    exec.exec(`asw s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 }
 
 run()
